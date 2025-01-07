3b9ddefe4b893461fc6c723ddecf035115658f29
amuif <amudihayat@gmail.com>
2025-01-07 15:29:36 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+
+        <div>
+          <h1>Amuif&apos;s Computer <br/> Repair Shop</h1>
+        </div>
+      </main>
+      
+    </div>
+  );
+}
